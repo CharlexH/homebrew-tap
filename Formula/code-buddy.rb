6757aa0aeead7d80cead2d7edb7980b0477723d7
CharlexH <zghzhhc@gmail.com>
2026-04-24 02:15:36 +0800

    code-buddy 0.1.2

--- Formula/code-buddy.rb
+++ Formula/code-buddy.rb
@@ -3,8 +3,8 @@ class CodeBuddy < Formula
 
   desc "StickS3 Bluetooth approvals for Codex CLI on macOS"
   homepage "https://github.com/CharlexH/CodeBuddy"
-  url "https://github.com/CharlexH/CodeBuddy/releases/download/v0.1.1/code-buddy-v0.1.1.tar.gz"
-  sha256 "01463a7bb16bf3ff2282a3216862477ab91ea5ff18cd25c32981f83565c65060"
+  url "https://github.com/CharlexH/CodeBuddy/releases/download/v0.1.2/code-buddy-v0.1.2.tar.gz"
+  sha256 "9eeb13de43a8a16ec240d91c160ca6b0cdb37bf593a9538b917f7d0ade852e3d"
   license "MIT"
 
   depends_on "python@3.13"
@@ -15,8 +15,8 @@ class CodeBuddy < Formula
   end
 
   resource "code-buddy-helper" do
-    url "https://github.com/CharlexH/CodeBuddy/releases/download/v0.1.1/code-buddy-macos-helper-v0.1.1.zip"
-    sha256 "ece9475608d940f325d0b8cc2ef49f4ea91f71fa5e57bb29bec33c2de7ec5316"
+    url "https://github.com/CharlexH/CodeBuddy/releases/download/v0.1.2/code-buddy-macos-helper-v0.1.2.zip"
+    sha256 "9250d0649b75bde77f5ea453c158ce86c57517cbe44bca9ea228b3fcd6b8a11b"
   end
 
   def install
@@ -32,7 +32,7 @@ class CodeBuddy < Formula
     system "ditto", "-x", "-k", helper_resource.cached_download, helper_dest
 
     (bin/"code-buddy").write_env_script libexec/"bin/code-buddy",
-      CODEX_BUDDY_BLE_BACKEND: "native",
+      CODEX_BUDDY_BLE_BACKEND:    "native",
       CODEX_BUDDY_BLE_HELPER_APP: helper_dest/"CodeBuddyBLEHelper.app"
   end
 
